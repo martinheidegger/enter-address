d5062105b8f2f4c5223ae91f517736482013578a
Martin Heidegger <martin.heidegger@gmail.com>
2016-01-23 18:00:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ $country.on('change', function () {
   console.log($country.val())
 })
 
+function done(count) {
+  if (count === 2) {
+    console.log(langs)
+  }  
+}
+
+var langs = {}
+var cnt=0
+['ja', 'en'].forEach(function (lang) {
+  $.get(lang + '.json', function (data) {
+    $.get('areas_' + lang + '.json', function (areas) {
+      data.areas = areas;
+      done(++cnt)
+    })
+  })
+})
+
 chooseCountry('JA');
